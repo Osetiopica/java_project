f7f4f35e19ec4072ccd93dba486fc51e518699c2
Osetiopica <osetiopica@gmail.com>
2021-05-18 15:14:03 +0900

    05/18-15:13

--- src/com/ict11/ojdbc/Ex03_DAO.java
+++ src/com/ict11/ojdbc/Ex03_DAO.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 public class Ex03_DAO {
 
 	Connection conn = null;
-	PreparedStatement pstmt = null;
+	PreparedStatement psmt = null;
 	ResultSet rs = null;
 	int result = 0;
 
@@ -39,10 +39,10 @@ public class Ex03_DAO {
 		try {
 			conn=getConnection();
 			String sql = "select * from book where bookid=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			rs = pstmt.executeQuery();
-			System.out.println("도서번호\t도서명\t출판사\t가격");
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, id);
+			rs = psmt.executeQuery();
+			System.out.println("번호\t도서명\t출판사\t가격");
 			while (rs.next()) {
 				System.out.print(rs.getInt("bookid") + "\t");
 				System.out.print(rs.getString("bookname") + "\t");
@@ -54,7 +54,7 @@ public class Ex03_DAO {
 		} finally {
 			try {
 				rs.close();
-				pstmt.close();
+				psmt.close();
 				conn.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
@@ -67,12 +67,12 @@ public class Ex03_DAO {
 		try {
 			conn=getConnection();
 			String sql = "insert into book values(?,?,?,?)";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bookid);
-			pstmt.setString(2, bookname);
-			pstmt.setString(3, publisher);
-			pstmt.setInt(4, price);
-			result = pstmt.executeUpdate();
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, bookid);
+			psmt.setString(2, bookname);
+			psmt.setString(3, publisher);
+			psmt.setInt(4, price);
+			result = psmt.executeUpdate();
 			if (result != 0) {
 				getList();
 			}
@@ -81,7 +81,7 @@ public class Ex03_DAO {
 		} finally {
 			try {
 				rs.close();
-				pstmt.close();
+				psmt.close();
 				conn.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
@@ -93,12 +93,12 @@ public class Ex03_DAO {
 		try {
 			conn=getConnection();
 			String sql = "update book set bookname=?, publisher=?, price=? where bookid=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, bookname);
-			pstmt.setString(2, publisher);
-			pstmt.setInt(3, price);
-			pstmt.setInt(4, bookid);
-			result = pstmt.executeUpdate();
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, bookname);
+			psmt.setString(2, publisher);
+			psmt.setInt(3, price);
+			psmt.setInt(4, bookid);
+			result = psmt.executeUpdate();
 			if (result != 0) {
 				getList();
 			}
@@ -107,7 +107,7 @@ public class Ex03_DAO {
 		} finally {
 			try {
 				rs.close();
-				pstmt.close();
+				psmt.close();
 				conn.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
@@ -119,9 +119,9 @@ public class Ex03_DAO {
 		try {
 			conn=getConnection();
 			String sql = "delete from book where bookid=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bookid);
-			result = pstmt.executeUpdate();
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, bookid);
+			result = psmt.executeUpdate();
 			if (result != 0) {
 				getList();
 			}
@@ -130,7 +130,7 @@ public class Ex03_DAO {
 		} finally {
 			try {
 				rs.close();
-				pstmt.close();
+				psmt.close();
 				conn.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
@@ -142,9 +142,9 @@ public class Ex03_DAO {
 		try {
 			conn=getConnection();
 			String sql = "select * from book order by bookid";
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			System.out.println("도서번호\t도서명\t출판사\t가격");
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			System.out.println("번호\t도서명\t출판사\t가격");
 			while (rs.next()) {
 				System.out.print(rs.getInt("bookid") + "\t");
 				System.out.print(rs.getString("bookname") + "\t");
@@ -156,7 +156,7 @@ public class Ex03_DAO {
 		} finally {
 			try {
 				rs.close();
-				pstmt.close();
+				psmt.close();
 				conn.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
